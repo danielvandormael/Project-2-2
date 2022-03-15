b820766765635c9dd5f2b9398425df8938ff8ca6
Grgur Damiani <damianigrgur@gmail.com>
2022-03-15 13:09:42 +0100

    Entity movement

--- Explorer/src/main/java/nl/maastrichtuniversity/dke/explorer/GUI/entities/Entity.java
+++ Explorer/src/main/java/nl/maastrichtuniversity/dke/explorer/GUI/entities/Entity.java
@@ -1,12 +1,16 @@
 package nl.maastrichtuniversity.dke.explorer.GUI.entities;
 
+import javafx.geometry.Point2D;
 import nl.maastrichtuniversity.dke.explorer.GUI.GamePanel;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
 
 public class Entity {
     public double x, y;
+    public Point2D position = new Point2D(x,y);
     public double viewAngle;
     public double viewRange;
     public double viewAngleSize;
@@ -32,6 +36,14 @@ public class Entity {
 
         this.direction = "down";
         pictureSwitch = 1;
+    }
+
+    public void walk(Point2D vector) {
+        this.position = position.add(vector);
+    }
+
+    public void rotate(double angle){
+        viewAngle += angle;
     }
 
     public BufferedImage getImage(){
@@ -72,5 +84,80 @@ public class Entity {
     }
 
 
+    public double getWidth() {return x;}
+
+    public double getHeight() {return y;}
+
+    public Point2D getPosition() {return position;}
+
+    public double getAngle() {return viewAngle;}
+
+    public Point2D getCenter() {
+        return new Point2D(position.getX() + x / 2, position.getY() + y / 2);
+    }
+
+
+// --------------------------------Movement--------------------------------------
+
+    private Point2D curVector = new Point2D(0, 0);
+    private float maxTorque = 5f;
+    private float curTorque = 0;
+
+    public void addMovement(double curSpeed) {
+        addMovement(curSpeed, getAngle());
+    }
+    private void addMovement(double curSpeed, double angle) {
+        Point2D thrustVector =  newVector(curSpeed, Math.toRadians(-angle));
+        curVector = curVector.add(thrustVector);
+        curVector = limit(curVector);
+    }
+
+    private Point2D limit(Point2D tempVector) {
+        if (tempVector.magnitude() > baseSpeed) {
+            return curVector = tempVector.normalize().multiply(baseSpeed);
+        } else {
+            return curVector = tempVector;
+        }
+    }
+
+    public void addTorque(float torqueForce) {
+        float newTorque = curTorque + torqueForce;
+        if (torqueForce > 0) {
+            curTorque = Math.min(newTorque, maxTorque);
+        } else {
+            maxTorque = Math.max(newTorque, -maxTorque);
+        }
+    }
+
+    private Point2D newVector(double scalar, double angle) {
+        return new Point2D(
+                (float) (Math.sin(angle) * baseSpeed),
+                (float) (Math.cos(angle) * baseSpeed));
+    }
+
+    private void applyDrag() {
+        float movementDrag = curVector.magnitude() < 0.5 ? 0.01f : 0.07f;
+        float rotationDrag = curTorque < 0.2f ? 0.05f : 0.1f;
+        curVector = new Point2D(
+                toZero((float) curVector.getX(), movementDrag),
+                toZero((float) curVector.getY(), movementDrag));
+        curTorque = toZero(curTorque, rotationDrag);
+    }
+
+    private float toZero(float value, float modifier) {
+        float newValue = 0;
+        if (value > modifier) {
+            newValue = value - modifier;
+        } else if (value < -modifier) {
+            newValue = value + modifier;
+        }
+        return newValue;
+    }
+
+    public void update() {
+        applyDrag();
+        walk(curVector);
+        rotate(curTorque);
+    }
 
 }
