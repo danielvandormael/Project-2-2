2808bb3b0af7b158fec8e40b8f9959224c562912
Diogo <diogorio45@gmail.com>
2022-04-25 19:50:30 +0200

    Update on marker type annotations

--- Explorer/src/main/java/nl/maastrichtuniversity/dke/explorer/Logic/Objects/ObjectManager.java
+++ Explorer/src/main/java/nl/maastrichtuniversity/dke/explorer/Logic/Objects/ObjectManager.java
@@ -17,11 +17,29 @@ public class ObjectManager {
 
     /*
         Types of Markers:
-        0 - Alert Marker
-        1 - Caution Marker
-        2 - Dead End Marker
-        3 - Finish Marker
-        4 - Unexplored Marker
+        ? - Alert Marker
+        ? - Caution Marker
+        (0) WARNING MARKER (for intruders only)
+            -> Whenever an intruder sees a guard (since it has a bigger viewing area), it should turn back and
+            leave a warning marker at that tile to warn the other intruder(s) that the guard had been checking
+            that area recently. Should wear off after a while, since it's understood the guard kept moving.
+        (1) DEAD END MARKER
+            -> Whenever a guard/intruder goes into a dead end (and recognizes), on its way back, it marks the
+            initial tile of that dead end.
+        ? - Unexplored Marker
+
+        * NEW POSSIBLE TYPES *
+        (2) TIME / STEP-BASED PHEROMONE
+            -> after x sec (or steps) from the tile the guard left the marker on,
+            it disappears, as it represents how recently that tile was covered.
+        (3) SMELL VARIATION (FROM TYPE 2)
+            -> each stepped-on tile is marked w/ 2, as explained. Similarly, each tile covered by the viewing area also
+            gets a marker to expand optimization. This will also wear out after x sec (or steps). These view-markers
+            can be thought of as the smell of the original pheromone 2.
+        (4) DEFINITE BY-WALL MARKER
+            -> if an agent has tracked a wall, every step on a tile immediately next to it is
+            marked. This is because it's an easy way of reducing any irrelevant re-tracks, since guards can more often
+            than not view the by-wall tiles from any adjacent tiles.
      */
 
     public ObjectManager(GamePanel gamePanel){
